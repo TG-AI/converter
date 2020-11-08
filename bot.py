01fa59650e53ae6a7e983108138942a35cf54194
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-11-08 21:01:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,11 @@ def help_command(update, context):
     update.message.reply_text('**Tutorial**\n\nHello This Bot Can Short Your Link\n\nFirst <b>YOU HAVE TO GET YOUR API TOKEN OF GPLINK FROM <\b>https://gplinks.in/member/tools/api \n\nAFTER THAT COPY THAT LINK FROM GPLINK TOOLS API\nIT WILL LOOK LIKE  https://gplinks.in/api?api=6a4cb74d70edd86803333333333a&\nSENT IT TO ME\n\nNOW YOU ARE DONE JUST SEND LINK TO THIS BOT \n\nNOW YOU CAN USE THIS BOT \nTHANKS FOR USING MY BOT \n\n')
 
 
-def cur_conv(update, context):
+@userge.on_cmd("cr", about={
+    'header': "use this to convert currency & get exchange rate",
+    'description': "Convert currency & get exchange rates.",
+    'examples': "{tr}cr 1 BTC USD"})
+async def cur_conv(message: Message):
     """
     this function can get exchange rate results
     """
