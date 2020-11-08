f13d450a23f455483237bb7d9fcabd06d01679eb
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-11-08 21:15:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,12 +19,11 @@ def help_command(update, context):
     update.message.reply_text('**Tutorial**\n\nHello This Bot Can Short Your Link\n\nFirst <b>YOU HAVE TO GET YOUR API TOKEN OF GPLINK FROM <\b>https://gplinks.in/member/tools/api \n\nAFTER THAT COPY THAT LINK FROM GPLINK TOOLS API\nIT WILL LOOK LIKE  https://gplinks.in/api?api=6a4cb74d70edd86803333333333a&\nSENT IT TO ME\n\nNOW YOU ARE DONE JUST SEND LINK TO THIS BOT \n\nNOW YOU CAN USE THIS BOT \nTHANKS FOR USING MY BOT \n\n')
 
     
-def cur_conv(update, context):
+async def cur_conv(bot, update):
     """
     this function can get exchange rate results
     """
-    if Config.CURRENCY_API is None:
-      async  
+    if Config.CURRENCY_API is None: 
         await message.edit(
             "<code>Oops!!get the API from</code> "
             "<a href='https://free.currencyconverterapi.com'>HERE</a> "
