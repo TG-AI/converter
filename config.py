ea19c77b0e8a32f79f19531a95ea84d63428cb73
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-11-09 18:28:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
